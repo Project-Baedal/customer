3d9f2cd6c88ee9b5020bd18137a938defd2ae36c
handsone-u <handsoneu@gmail.com>
2025-04-13 23:21:59 +0900

    [#2] del: jwt 토큰 생성은 gateway 필터에서 처리하도록 하기 때문에 호출 따로 필요 없음

--- src/main/java/com/baedal/customer/application/port/out/TokenServicePort.java
+++
@@ -1,8 +0,0 @@
-package com.baedal.customer.application.port.out;
-
-public interface TokenServicePort {
-
-  String requestToken(String email);
-
-  Boolean validateToken(String token);
-}
--- src/main/java/com/baedal/customer/application/service/CustomerAuthenticationService.java
+++ src/main/java/com/baedal/customer/application/service/CustomerAuthenticationService.java
@@ -1,7 +1,6 @@
 package com.baedal.customer.application.service;
 
 import com.baedal.customer.application.port.in.CustomerAuthenticationUsecase;
-import com.baedal.customer.application.port.out.TokenServicePort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,7 +16,6 @@ public class CustomerAuthenticationService implements CustomerAuthenticationUsec
 
   private final PasswordEncoder passwordEncoder;
   private final UserDetailsService userDetailsService;
-  private final TokenServicePort tokenServicePort;
 
   @Transactional(readOnly = true)
   public String authenticate(String email, String password) {

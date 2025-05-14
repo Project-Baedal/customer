5f28093bbcb0679064702516722dc23a6d5370b5
handsone-u <handsoneu@gmail.com>
2025-05-14 12:29:17 +0900

    [#9] add: 서비스 메서드 실행 시간 로깅 Aspect

--- src/main/java/com/baedal/customer/application/service/CustomerService.java
+++ src/main/java/com/baedal/customer/application/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.baedal.customer.application.service;
 
+import com.baedal.customer.application.annotation.LogExecutionTime;
 import com.baedal.customer.application.mapper.CustomerApplicationMapper;
 import com.baedal.customer.application.port.dto.CustomerInfo;
 import com.baedal.customer.domain.entity.Customer;
@@ -17,6 +18,7 @@ public class CustomerService {
 
   private final CustomerRepository repository;
 
+  @LogExecutionTime
   @Transactional(readOnly = true)
   public CustomerInfo getCustomer(Long customerId) {
     Customer customer = repository.findById(customerId)
@@ -24,6 +26,7 @@ public class CustomerService {
     return mapper.entityToInfo(customer);
   }
 
+  @LogExecutionTime
   @Transactional(readOnly = true)
   public Collection<CustomerInfo> getCustomers(Collection<Long> customerIds) {
     return repository.findAllById(customerIds)
